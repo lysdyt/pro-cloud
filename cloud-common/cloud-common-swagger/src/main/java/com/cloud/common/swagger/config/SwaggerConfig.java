e166edab67f0f053e453fd6e744b499e30de97e5
Crazy <2929783435qq.com>
2020-01-03 19:00:36 +0800

    swagger-ui 升级

--- cloud-common/cloud-common-swagger/src/main/java/com/cloud/common/swagger/config/SwaggerConfig.java
+++ cloud-common/cloud-common-swagger/src/main/java/com/cloud/common/swagger/config/SwaggerConfig.java
@@ -1,12 +1,14 @@
 package com.cloud.common.swagger.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableSwaggerBootstrapUi;
 import com.google.common.collect.Lists;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.*;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -14,7 +16,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -24,6 +25,8 @@ import java.util.List;
  */
 @Configuration
 @EnableSwagger2
+@EnableSwaggerBootstrapUi
+@Import(BeanValidatorPluginsConfiguration.class)
 @ConditionalOnProperty(name = "swagger.enabled", matchIfMissing = true )
 public class SwaggerConfig {
 
